a743b49b542f8bd0d1e39cb76e74482fbffc5fc0
djdt <djdt@djdt.djdt>
2017-04-03 14:13:25 +1000

    fix width

--- src/ArgsParseOption.cpp
+++ src/ArgsParseOption.cpp
@@ -1,23 +1,33 @@
 #include "ArgsParseOption.hpp"
 
+#include <algorithm>
 #include <iomanip>
 
 namespace argsparse {
 
 	std::ostream& operator<<(std::ostream& os, const Option& opt)
 	{
-		size_t init_w = os.width();
+		int32_t w = 0;
 		if (opt.short_opt != 0) {
 			os << "-" << opt.short_opt << ",";
+			w += 3;
 		}
+
 		os << "--" << opt.long_opt << " ";
+		w += (3 + opt.long_opt.length());
+
 		if (opt.nargs) {
 			std::string upper(opt.long_opt);
 			std::transform(upper.begin(), upper.end(), upper.begin(),
 					[] (char c) { return std::toupper(c); });
-			opt.nargs > 1 ? os << upper << "..." : os << upper;
+			os << upper;
+			w += upper.length();
 		}
-		return os << std::setw(30 - (os.width() - init_w)) << opt.desc;
+		if (opt.nargs > 1) {
+			os << "...";
+			w += 3;
+		}
+		return os << " " << std::setw(29 - w) << opt.desc;
 	}
 
 } /* namespace argsparse */
