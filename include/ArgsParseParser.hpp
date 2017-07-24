b15519dd1135715a49a8375be4153fc1ae5d6d6a
djdt <djdt@djdt.djdt>
2017-07-24 12:30:06 +1000

    some documentation

--- include/ArgsParseParser.hpp
+++ include/ArgsParseParser.hpp
@@ -1,6 +1,27 @@
 #ifndef _ARGS_PARSE_PARSER_H_
 #define _ARGS_PARSE_PARSER_H_
 
+/* args_parse Parser class:
+ * 
+ * Initialise with an optional program name Parser parser(<PROG_NAME>)
+ * 
+ * Add any options / args required using addOption and addPositional.
+ * .addOption<TYPE>(<LONG_NAME>, <CHAR>, <HELP_DESC>,
+ * .addPositional(<NAME>).
+ * If not a simple switch (true/false) use:
+ * .addOption<TYPE>(<LONG_NAME>, <CHAR>, <HELP_DESC>,
+ *                  <DEFAULT_VAL>, <NUM_REQ_ARGS>)
+ * 
+ * Parse input using .parse(argc, argv).
+ * 
+ * Options can be accessed using [<NAME>] (returns true if used)
+ * or .option<TYPE>(<NAME>) that will return the value casted as <TYPE>.
+ * Positional accessed using .positional(<NAME>).
+ * 
+ * Passing the parser object to a stream will print generated help text
+ * Additional text to printed can be added using .addText(<TEXT>).
+ */
+
 #include <map>
 #include <string>
 #include <vector>
